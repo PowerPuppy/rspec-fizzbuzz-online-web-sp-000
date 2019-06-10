8ecd71a9f8b75cffe9a4a5d6b6278e6e0df202f0
Chris Cheong <chrisxcheong@gmail.com>
2019-06-10 21:53:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num/3 == int && num/5 == int
+    puts "FizzBuzz"
+  elsif num/5 == int
+    puts "Buzz"
+  elsif num/3 == int
+    puts "Fizz"
+  else puts "nil"
+    end
+
+end
