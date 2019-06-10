52d36225ea6418f697c6279b318ee277037512ce
Chris Cheong <chrisxcheong@gmail.com>
2019-06-10 22:04:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(cyber)
-  if cyber % 15  == 0
+def fizzbuzz(num)
+  if num % 15  == 0
     puts "FizzBuzz"
-  elsif cyber % 5 == 0
+  elsif num % 5 == 0
     puts "Buzz"
-  elsif cyber % 3 == 0
+  elsif num % 3 == 0
     puts "Fizz"
   else puts "nil"
     end
-
 end
